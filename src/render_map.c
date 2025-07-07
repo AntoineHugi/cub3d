5d7775100e1a52f9a55bab035358a2078e574d26
AHugi <antoine.hugi@gmail.com>
2025-07-07 16:02:39 +0200

    rendering steps

--- src/render_map.c
+++ src/render_map.c
@@ -2,6 +2,12 @@
 /*
 void	render_map(t_game *game)
 {
-	// define field of view (FOV) and create #rays = screen width
+	Cast rays per vertical column of pixels to find:
+   		The distance to the nearest wall in that direction.
+		The exact position on the wall texture to sample for that vertical slice.
+	Calculate the height of the wall slice on screen, based on the distance.
+	Draw that vertical slice of wall at the correct height in the column.
+	Fill everything above the wall slice with the ceiling color.
+	Fill everything below the wall slice with the floor color.
 }
 */
